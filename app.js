a092f7a7260814d77b499add74bc7847743d4175
Arshi <arshikhan81099@gmail.com>
2024-07-29 16:55:21 +0530

    adding error

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const Listing = require("./models/listing");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync");
+const expessError = require("./utils/expressError");
 
 const MONGO_URL = "mongodb://localhost:27017/wanderlust";
 
@@ -28,10 +30,10 @@ app.get("/", (req, res) => {
     });
 
 // Index Route
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
-});
+}));
 
 // New Route
 app.get("/listing/new", (req, res) => {
@@ -39,39 +41,55 @@ app.get("/listing/new", (req, res) => {
 });
 
 // Show Route
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", { listing });
-})
+}));
 
 // Create Route
-app.post("/listing", async(req, res) => {
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-});
+app.post("/listing", wrapAsync(async(req, res, next) => {
+        if(!req.body.listing){
+            throw new expessError(400, "Invalid Listing Data");
+        }
+        const newListing = new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/listings");
+}));
 
 // Edit Route
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", { listing });
-});
+}));
 
 // Update Route
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", wrapAsync(async (req, res) => {
+    if(!req.body.listing){
+        throw new expessError(400, "Invalid Listing Data");
+    }
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
-});
+}));
 
 // Delete Route
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync(async(req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
+}));
+
+app.all("*", (req, res, next) => {
+    next(new expessError(404, "Page Not Found"));
+});
+
+app.use((err, req, res, next) => {
+    let { statusCode = 500, message = "Something went wrong!!"} = err;
+    res.status(statusCode).render("error.ejs", { err });
+    // res.render("error.ejs"), { err };
 });
 
 app.listen(8080, () => {
---
+++ utils/expressError.js
@@ -0,0 +1,9 @@
+class expessError extends Error {
+    constructor(message, statusCode) {
+        super();
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+}
+
+module.exports = expessError;
