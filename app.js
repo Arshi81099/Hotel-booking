51655008a9eb364d8a6b55d179b0f78f654b60f7
Arshi <arshikhan81099@gmail.com>
2024-08-06 22:11:40 +0530

    login

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const ejsMate = require("ejs-mate");
 const expressError = require("./utils/expressError");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 
 const MONGO_URL = "mongodb://localhost:27017/wanderlust";
@@ -43,6 +47,13 @@ const sessionOptions = {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
@@ -53,8 +64,9 @@ app.get("/", (req, res) => {
     res.send("Hii! I am root");
     });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 app.all("*", (req, res, next) => {
     next(new expressError(404, "Page Not Found"));
---
+++ routes/user.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();   
+const User = require('../models/user');
+const wrapAsync = require('../utils/wrapAsync');
+const passport = require('passport');
+
+router.get('/signup', (req, res) => {
+    res.render('users/signup');
+});
+
+
+router.post('/signup', wrapAsync(async (req, res) => {
+    let { email, username, password } = req.body;
+    const newUser = new User({ email, username });
+    const registeredUser = await User.register(newUser, password);
+    req.flash("success", "Welcome to Wanderlust!");
+    res.redirect('/listings');
+}));
+
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.post('/login',
+    passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),
+    async (req, res) => {
+    req.flash("success", "Welcome back!");
+    res.redirect('/listings');
+});
+
+module.exports = router;
