9c378314ec15e74b4041879826d3df10d4beb86c
Arshi <arshikhan81099@gmail.com>
2024-08-06 19:43:01 +0530

    flash styling added

--- routes/listing.js
+++ routes/listing.js
@@ -54,6 +54,7 @@ router.get("/:id/edit", wrapAsync(async (req, res) => {
 router.put("/:id", validateListing, wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    req.flash("success", "Successfully updated the listing");
     res.redirect(`/listings/${id}`);
 }));
 
@@ -62,6 +63,7 @@ router.delete("/:id", wrapAsync(async(req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
+    req.flash("success", "Successfully deleted the listing");
     res.redirect("/listings");
 }));
 
--- routes/review.js
+++ routes/review.js
@@ -28,6 +28,7 @@ router.post("/", validateReview, wrapAsync(async (req, res) => {
     await newReview.save();
     await listing.save();
 
+    req.flash("success", "Successfully created a new review");
     res.redirect(`/listings/${listing._id}`);
 }));
 
@@ -36,6 +37,8 @@ router.delete("/:reviewId", wrapAsync(async (req, res) => {
     let { id, reviewId } = req.params;
     await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
+
+    req.flash("success", "Successfully deleted the review");
     res.redirect(`/listings/${id}`);
 }));
 
